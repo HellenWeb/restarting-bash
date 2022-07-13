d03bc4a9c670612f8d52ece08270ac5081565280
HellenWeb <artemkondratyev23@gmail.com>
2022-07-13 17:39:17 +0300

    Added main.sh file and added changes in there

---
+++ main.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+red='\033[1;31m'
+rset='\033[0m'
+grn='\033[1;32m'
+ylo='\033[1;33m'
+blue='\033[1;34m'
+cyan='\033[1;36m'
+pink='\033[1;35m'
+
+restart_service() {
+    echo ""
+    while true; do
+        echo -n -e "$rset Enter util (Cancel: 1):$rset "; read util
+        if [[ $util == "1" ]]; then
+            break
+        fi
+        array[${#array[@]}]=$util
+    done
+    echo ""
+    for i in ${array[@]}; do
+        echo -e "$grn[*]$grn $ylo Restarting $i$ylo $red"; service $i restart 
+    done
+}
+
+ls_catalog() {
+    echo ""
+    echo -e "$grn[*]$grn $ylo Show everything in the catalog $name$ylo";
+    ls $name -a
+}
+
+echo ""
+echo -e "$grn             ██████╗░███████╗░██████╗████████╗░█████╗░██████╗░████████╗██╗███╗░░██╗░██████╗░░░░░░░░░░
+             ██╔══██╗██╔════╝██╔════╝╚══██╔══╝██╔══██╗██╔══██╗╚══██╔══╝██║████╗░██║██╔════╝░░░░░░░░░░
+             ██████╔╝█████╗░░╚█████╗░░░░██║░░░███████║██████╔╝░░░██║░░░██║██╔██╗██║██║░░██╗░░░░░░░░░░
+             ██╔══██╗██╔══╝░░░╚═══██╗░░░██║░░░██╔══██║██╔══██╗░░░██║░░░██║██║╚████║██║░░╚██╗░░░░░░░░░
+             ██║░░██║███████╗██████╔╝░░░██║░░░██║░░██║██║░░██║░░░██║░░░██║██║░╚███║╚██████╔╝██╗██╗██╗
+             ╚═╝░░╚═╝╚══════╝╚═════╝░░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝╚═╝░░╚══╝░╚═════╝░╚═╝╚═╝╚═╝
+                                        Copyright by. Hellen$grn"
+echo ""
+echo -n -e "$blue Enter function [1. Restarting utils; 2. Show all in catalog]:$blue "; read input
+
+if [[ $input == '1' ]]; then
+    restart_service
+elif [[ $input == '2' ]]; then
+    echo ""
+    echo -n -e "$blue Enter catalog: $blue"; read etc
+    name=$etc
+    ls_catalog
+fi
